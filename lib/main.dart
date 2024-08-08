c5ffd040242d51f941a9be8e9e400f7334413c18
Yujin <tkfkd7038@naver.com>
2024-08-08 15:24:08 +0900

    feat: ItemListPage 구현 후주석 해제

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_mall/item_list_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Shopping App',
-      //home: ItemListPage(),
+      home: const ItemListPage(),
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
