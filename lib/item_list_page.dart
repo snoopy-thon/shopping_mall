f2fbafcb05a9e39b5ca5ea485c5ce107e8d3b6d8
Yujin <tkfkd7038@naver.com>
2024-08-08 14:51:25 +0900

    feat: item 리스트 페이지에 아이템 정보 추가

---
+++ lib/item_list_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:shopping_mall/models/product.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class ItemListPage extends StatefulWidget {
+  const ItemListPage({super.key});
+
+  @override
+  State<ItemListPage> createState() => _ItemListPageState();
+}
+
+class _ItemListPageState extends State<ItemListPage> {
+  final NumberFormat numberFormat = NumberFormat('###,###,###,###');
+  List<Product> productList = [
+    Product(
+      productNo: 1,
+      productName: "노트북(Laptop)",
+      productImageUrl: "https://picsum.photos/id/1/300/300",
+      price: 600000,
+    ),
+    Product(
+      productNo: 2,
+      productName: "스마트폰(Phone)",
+      productImageUrl: "https://picsum.photos/id/20/300/300",
+      price: 500000,
+    ),
+    Product(
+      productNo: 3,
+      productName: "머그컵(Cup)",
+      productImageUrl: "https://picsum.photos/id/30/300/300",
+      price: 15000,
+    ),
+    Product(
+      productNo: 4,
+      productName: "키보드(Keyboard)",
+      productImageUrl: "https://picsum.photos/id/60/300/300",
+      price: 50000,
+    ),
+    Product(
+      productNo: 5,
+      productName: "포도(Grape)",
+      productImageUrl: "https://picsum.photos/id/75/200/300",
+      price: 75000,
+    ),
+    Product(
+      productNo: 6,
+      productName: "책(book)",
+      productImageUrl: "https://picsum.photos/id/24/200/300",
+      price: 24000,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('제품 리스트'),
+        centerTitle: true,
+      ),
+    );
+  }
+}
