fe95b340f0893de6f4f3bbc091131217372181f0
Yujin <tkfkd7038@naver.com>
2024-08-08 19:36:57 +0900

    feat: basketContainer 구현 및 장바구니 상품 화면에 보여주는 UI 구현

--- lib/item_basket_page.dart
+++ lib/item_basket_page.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:shopping_mall/constants.dart';
-
-import 'models/product.dart';
+import 'package:shopping_mall/models/product.dart';
 
 class ItemBasketPage extends StatefulWidget {
   const ItemBasketPage({super.key});
@@ -20,16 +19,16 @@ class _ItemBasketPageState extends State<ItemBasketPage> {
       price: 600000,
     ),
     Product(
-      productNo: 2,
-      productName: "스마트폰(Phone)",
-      productImageUrl: "https://picsum.photos/id/20/300/300",
-      price: 500000,
+      productNo: 4,
+      productName: "키보드(Keyboard)",
+      productImageUrl: "https://picsum.photos/id/60/300/300",
+      price: 50000,
     ),
   ];
 
   List<Map<int, int>> quantityList = [
     {1: 2},
-    {2: 3},
+    {4: 3},
   ];
 
   double totalPrice = 0;
@@ -50,7 +49,105 @@ class _ItemBasketPageState extends State<ItemBasketPage> {
         title: const Text("장바구니"),
         centerTitle: true,
       ),
-      body: Container(),
+      body: ListView.builder(
+        itemCount: basketList.length,
+        itemBuilder: (context, index) {
+          return basketContainer(
+            productNo: basketList[index].productNo ?? 0,
+            productName: basketList[index].productName ?? "",
+            productImageUrl: basketList[index].productImageUrl ?? "",
+            price: basketList[index].price ?? 0,
+            quantity: quantityList[index][basketList[index].productNo] ?? 0,
+          );
+        },
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(20),
+        child: FilledButton(
+          onPressed: () {},
+          child: Text("총 ${numberFormat.format(totalPrice)}원 결제하기"),
+        ),
+      ),
+    );
+  }
+
+  Widget basketContainer({
+    required int productNo,
+    required String productName,
+    required String productImageUrl,
+    required double price,
+    required int quantity,
+  }) {
+    return Container(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CachedNetworkImage(
+            width: MediaQuery.of(context).size.width * 0.3,
+            fit: BoxFit.cover,
+            imageUrl: productImageUrl,
+            placeholder: (context, url) {
+              return const Center(
+                child: CircularProgressIndicator(
+                  strokeWidth: 2,
+                ),
+              );
+            },
+            errorWidget: (context, url, error) {
+              return const Center(
+                child: Text("오류 발생"),
+              );
+            },
+          ),
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  productName,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "${numberFormat.format(price)}원",
+                ),
+                Row(
+                  children: [
+                    const Text("수량: "),
+                    IconButton(
+                        onPressed: () {},
+                        icon: const Icon(
+                          Icons.remove,
+                        )),
+                    Text(
+                      "$quantity",
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.add,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.delete,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  "합계: ${numberFormat.format(price * quantity)}원",
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
