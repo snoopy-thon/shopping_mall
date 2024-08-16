fb2b41ce2ac9a4fbd9bb79353c101a07f669afc2
Yujin <tkfkd7038@naver.com>
2024-08-16 17:16:38 +0900

    refactor: 제품 이미지, 이름, 가격을 위젯으로 분리하기

--- lib/item_details_page.dart
+++ lib/item_details_page.dart
@@ -24,6 +24,7 @@ class ItemDetailPage extends StatefulWidget {
 }
 
 class _ItemDetailPageState extends State<ItemDetailPage> {
+  int quantity = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,45 +35,9 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Container(
-              alignment: Alignment.center,
-              padding: const EdgeInsets.all(15),
-              child: CachedNetworkImage(
-                width: MediaQuery.of(context).size.width * 0.8,
-                imageUrl: widget.productImageUrl,
-                placeholder: (context, url) {
-                  return const Center(
-                    child: CircularProgressIndicator(
-                      strokeWidth: 2,
-                    ),
-                  );
-                },
-                errorWidget: (context, url, error) {
-                  return const Center(
-                    child: Text("오류 발생"),
-                  );
-                },
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20,
-              ),
-              child: Text(
-                widget.productName,
-                style: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.symmetric(
-                vertical: 20,
-              ),
-              child: Text(
-                "${numberFormat.format(widget.price)}원",
-              ),
-            ),
+            productImageContainer(),
+            productNameContainer(),
+            productPriceContainer(),
           ],
         ),
       ),
@@ -90,6 +55,54 @@ class _ItemDetailPageState extends State<ItemDetailPage> {
           },
           child: const Text("장바구니 담기"),
         ),
+      ),
+    );
+  }
+
+  Widget productPriceContainer() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+      ),
+      child: Text(
+        "${numberFormat.format(widget.price)}원",
+      ),
+    );
+  }
+
+  Widget productNameContainer() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        vertical: 20,
+      ),
+      child: Text(
+        widget.productName,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget productImageContainer() {
+    return Container(
+      alignment: Alignment.center,
+      padding: const EdgeInsets.all(15),
+      child: CachedNetworkImage(
+        width: MediaQuery.of(context).size.width * 0.8,
+        imageUrl: widget.productImageUrl,
+        placeholder: (context, url) {
+          return const Center(
+            child: CircularProgressIndicator(
+              strokeWidth: 2,
+            ),
+          );
+        },
+        errorWidget: (context, url, error) {
+          return const Center(
+            child: Text("오류 발생"),
+          );
+        },
       ),
     );
   }
